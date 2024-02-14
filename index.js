88be11f9dc0378f4402be477265417c1ffaf1c80
gawston <naruenat.privatez@gmail.com>
2024-02-14 10:24:06 +0700

    update

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 
 const express = require('express');
 const axios = require('axios');
-const app = express();
 var bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 
 // Base URL for the API
 // const base_url = 'http://localhost:3000';
 const base_url = 'http://node57059-naruenat-noderest.proen.app.ruk-com.cloud';
 
 // Set the template engine
+app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
